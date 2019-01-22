98dfe327883a5bbdedadcd9401926801f8822140
wert3232 <275129737@qq.com>
2019-01-22 14:22:57 +0800

    2019/1/22

--- library/src/main/java/cn/qqtheme/framework/picker/FilePicker.kt
+++ library/src/main/java/cn/qqtheme/framework/picker/FilePicker.kt
@@ -1,23 +1,16 @@
 package cn.qqtheme.framework.picker
 
 import android.app.Activity
-import android.content.res.ColorStateList
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
-import android.os.Build
 import android.support.annotation.IntDef
 import android.support.constraint.ConstraintLayout
 import android.support.constraint.ConstraintSet
-import android.support.v4.content.ContextCompat
 import android.support.v7.content.res.AppCompatResources
 import android.view.Gravity
 import android.view.View
-import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.LinearLayout
-import android.widget.ListView
-import android.widget.TextView
+import android.widget.*
 
 import com.common.*
 import com.library.R
@@ -27,7 +20,6 @@ import java.lang.annotation.RetentionPolicy
 
 import cn.qqtheme.framework.adapter.FileAdapter
 import cn.qqtheme.framework.adapter.PathAdapter
-import cn.qqtheme.framework.entity.FileItem
 import cn.qqtheme.framework.popup.BasicPopup
 import cn.qqtheme.framework.popup.ConfirmPopup
 import cn.qqtheme.framework.util.ConvertUtils
@@ -41,13 +33,7 @@ import cn.qqtheme.framework.widget.HorizontalListView
  * @author 李玉江[QQ:1032694760]
  * @since 2015/9/29, 2017/01/01, 2017/01/08
  */
-class FilePicker
-/**
- * @see .FILE
- *
- * @see .DIRECTORY
- */
-(activity: Activity, @param:Mode private val mode: Int) : ConfirmPopup<LinearLayout>(activity), AdapterView.OnItemClickListener {
+class FilePicker(activity: Activity, @param:Mode private val mode: Int) : ConfirmPopup<LinearLayout>(activity), AdapterView.OnItemClickListener {
 
     private var initPath: String? = null
     val adapter = FileAdapter()
@@ -83,123 +69,95 @@ class FilePicker
         return this
     }
 
-    //    @Override
-    //    @NonNull
-    //    protected LinearLayout makeCenterView() {
-    //        LinearLayout rootLayout = new LinearLayout(activity);
-    //        rootLayout.setLayoutParams(new LinearLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT));
-    //        rootLayout.setOrientation(LinearLayout.HORIZONTAL);
-    //        rootLayout.setBackgroundColor(Color.WHITE);
-    //
-    //        LinearLayout side = new LinearLayout(activity);
-    //        side.setLayoutParams(new LinearLayout.LayoutParams(200, MATCH_PARENT));
-    //        side.setOrientation(LinearLayout.HORIZONTAL);
-    //        side.setBackgroundColor(Color.BLACK);
-    //        rootLayout.addView(side);
-    //
-    //        ListView listView = new ListView(activity);
-    //        listView.setBackgroundColor(Color.WHITE);
-    //        listView.setDivider(new ColorDrawable(0xFFDDDDDD));
-    //        listView.setDividerHeight(1);
-    //        listView.setCacheColorHint(Color.TRANSPARENT);
-    //        LinearLayout.LayoutParams listViewLayoutParams = new LinearLayout.LayoutParams(0, WRAP_CONTENT);
-    //        listViewLayoutParams.weight = 1;
-    //        listView.setLayoutParams(listViewLayoutParams);
-    //        listView.setAdapter(adapter);
-    //        listView.setOnItemClickListener(this);
-    //        rootLayout.addView(listView);
-    //
-    //        emptyText = new TextView(activity);
-    //        LinearLayout.LayoutParams txtParams = new LinearLayout.LayoutParams(0, MATCH_PARENT);
-    //        txtParams.weight = 1;
-    //        emptyText.setLayoutParams(txtParams);
-    //        emptyText.setGravity(Gravity.CENTER);
-    //        emptyText.setVisibility(View.GONE);
-    //        emptyText.setTextColor(Color.BLACK);
-    //        emptyText.setBackgroundColor(Color.GREEN);
-    //        if(emptyView == null){
-    //            emptyView = emptyText;
-    //        }
-    //        rootLayout.addView(emptyView);
-    //
-    //        return rootLayout;
-    //    }
-
     override fun makeCenterView(): View {
         val rootLayout = ConstraintLayout(activity)
         rootLayout.id = View.generateViewId()
         rootLayout.layoutParams = ConstraintLayout.LayoutParams(BasicPopup.MATCH_PARENT, BasicPopup.MATCH_PARENT)
         rootLayout.setBackgroundColor(0x11000000)
 
-        val side = LinearLayout(activity)
-        val sideParams = ConstraintLayout.LayoutParams(200, BasicPopup.MATCH_PARENT)
-        sideParams.setMargins(0, 16, 16, 16)
-        sideParams.topToTop = ConstraintSet.PARENT_ID
-        sideParams.startToStart = ConstraintSet.PARENT_ID
-        sideParams.bottomToBottom = ConstraintSet.PARENT_ID
-        side.layoutParams = sideParams
-        side.gravity = Gravity.CENTER_HORIZONTAL
-        side.orientation = LinearLayout.VERTICAL
-        side.id = View.generateViewId()
-        val list = activity.storageList()
-        if (list != null) {
-            for (i in list.indices) {
-                val path = list[i]
-                val text = TextView(activity)
-                val params = LinearLayout.LayoutParams(BasicPopup.MATCH_PARENT, BasicPopup.WRAP_CONTENT)
-                params.topMargin = 4
-                params.bottomMargin = 4
-                text.setTextColor(AppCompatResources.getColorStateList(activity, R.color.storage_text_color))
-                text.text = (i + 1).toString() + ""
-                text.gravity = Gravity.CENTER
-                val drawable = AppCompatResources.getDrawable(activity, R.drawable.storage)
-                if (drawable != null) {
-                    drawable.setBounds(0, 0, 160, 160)
-                    text.setCompoundDrawables(null, drawable, null, null)
-                }
-                text.layoutParams = params
-                text.setOnClickListener { setDirectoryPath(path) }
-                side.addView(text)
+        val side = ScrollView(activity).apply {
+            id = View.generateViewId()
+            layoutParams = ConstraintLayout.LayoutParams(240, BasicPopup.MATCH_PARENT).apply {
+                setMargins(16, 16, 16, 16)
+                topToTop = ConstraintSet.PARENT_ID
+                startToStart = ConstraintSet.PARENT_ID
+                bottomToBottom = ConstraintSet.PARENT_ID
             }
         }
+        val storageParent = LinearLayout(activity).apply {
+            id = View.generateViewId()
+            gravity = Gravity.CENTER_HORIZONTAL
+            orientation = LinearLayout.VERTICAL
+            layoutParams = FrameLayout.LayoutParams(MATCH_PARENT, BasicPopup.WRAP_CONTENT)
+        }
+        side.addView(storageParent)
+
+        activity.storageList()?.also {list ->
+            for (i in list.reversed().indices) {
+                val path = list[i]
+                val tx = TextView(activity).apply {
+                    setTextColor(AppCompatResources.getColorStateList(activity, R.color.storage_text_color))
+                    setBackgroundColor(Color.WHITE)
+                    text = "${i + 1}"
+                    gravity = Gravity.CENTER
+                    layoutParams = LinearLayout.LayoutParams(BasicPopup.MATCH_PARENT, BasicPopup.WRAP_CONTENT).apply {
+                        topMargin = if(i == 0) 0  else 10
+                        bottomMargin = 10
+                    }
+                    setCompatElevation(8f)
+                }
+                AppCompatResources.getDrawable(activity, R.drawable.storage)?.also {img ->
+                    img.setBounds(0, 0, 150, 150)
+                    tx.setCompoundDrawables(null, img, null, null)
+                }
+
+                tx.setOnClickListener { setDirectoryPath(path) }
+                storageParent.addView(tx)
+            }
+        }
+
         rootLayout.addView(side)
 
-        val listView = ListView(activity)
-        listView.id = View.generateViewId()
-        listView.setBackgroundColor(Color.WHITE)
-        listView.divider = ColorDrawable(-0x222223)
-        listView.dividerHeight = 1
-        listView.cacheColorHint = Color.TRANSPARENT
-        val listViewLayoutParams = ConstraintLayout.LayoutParams(0, BasicPopup.MATCH_PARENT)
-        listViewLayoutParams.setMargins(16, 16, 16, 16)
-        listViewLayoutParams.topToTop = ConstraintSet.PARENT_ID
-        listViewLayoutParams.bottomToBottom = ConstraintSet.PARENT_ID
-        listViewLayoutParams.startToEnd = side.id
-        listViewLayoutParams.endToEnd = ConstraintSet.PARENT_ID
-        listView.layoutParams = listViewLayoutParams
+        val listView = ListView(activity).apply {
+            id = View.generateViewId()
+            setBackgroundColor(Color.WHITE)
+            divider = ColorDrawable(-0x222223)
+            dividerHeight = 1
+            cacheColorHint = Color.TRANSPARENT
+            setCompatElevation(8f)
+            layoutParams = ConstraintLayout.LayoutParams(0, BasicPopup.MATCH_PARENT).apply {
+                setMargins(16, 16, 16, 16)
+                topToTop = ConstraintSet.PARENT_ID
+                bottomToBottom = ConstraintSet.PARENT_ID
+                startToEnd = side.id
+                endToEnd = ConstraintSet.PARENT_ID
+            }
+        }
         listView.adapter = adapter
         listView.onItemClickListener = this
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            listView.elevation = 10f
-        }
         rootLayout.addView(listView)
 
-        emptyText = TextView(activity)
-        val txtParams = ConstraintLayout.LayoutParams(0, BasicPopup.MATCH_PARENT)
-        txtParams.setMargins(16, 16, 16, 16)
-        emptyText!!.gravity = Gravity.CENTER
-        emptyText!!.visibility = View.GONE
-        emptyText!!.setTextColor(Color.BLACK)
-        emptyText!!.setBackgroundColor(Color.WHITE)
+        emptyText = TextView(activity).apply {
+            gravity = Gravity.CENTER
+            visibility = View.GONE
+            setTextColor(Color.BLACK)
+            setBackgroundColor(Color.WHITE)
+        }
         if (emptyView == null) {
             emptyView = emptyText
         }
-        emptyView!!.layoutParams = txtParams
-        txtParams.topToTop = ConstraintSet.PARENT_ID
-        txtParams.bottomToBottom = ConstraintSet.PARENT_ID
-        txtParams.startToEnd = side.id
-        txtParams.endToEnd = ConstraintSet.PARENT_ID
-        emptyView!!.id = View.generateViewId()
+        emptyView?.apply {
+            id = View.generateViewId()
+            setCompatElevation(8f)
+            layoutParams = ConstraintLayout.LayoutParams(0, BasicPopup.MATCH_PARENT).apply {
+                setMargins(16, 16, 16, 16)
+                topToTop = ConstraintSet.PARENT_ID
+                bottomToBottom = ConstraintSet.PARENT_ID
+                startToEnd = side.id
+                endToEnd = ConstraintSet.PARENT_ID
+            }
+        }
+
         rootLayout.addView(emptyView)
 
         return rootLayout
